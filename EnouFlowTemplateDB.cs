44561cf448c7f77e196290605d143dc50d4227cc
shibamo <qinchao@hotmail.com>
2017-03-17 13:20:37 -0700

    增加实体类FlowDynamicUser与对应表定义, 代表可计算获得的用户的脚本
    运行其中的script字段内容将返回User列表,
    主要用于引擎执行时通过流程实例对象,表单对象,组织结构对象集以及表里定义的script脚本动态确定活动任务的接收人

--- EnouFlowTemplateDB.cs
+++ EnouFlowTemplateDB.cs
@@ -63,6 +63,7 @@ namespace EnouFlowTemplateLib
     #endregion
 
     public DbSet<FlowTemplate> flowTemplates { get; set; }
+    public DbSet<FlowDynamicUser> flowDynamicUsers { get; set; }
   }
 
   [Table("Enou_FlowTemplate")]
@@ -90,5 +91,62 @@ namespace EnouFlowTemplateLib
     public bool isPublished { get; set; } = false;
   }
 
-
+  /// <summary>
+  /// 可获得的用户的脚本, 运行script字段将返回User列表,
+  /// 主要用于引擎执行时通过流程实例对象,表单对象,组织结构对象集
+  /// 以及表里定义的script脚本动态确定活动任务的接收人
+  /// </summary>
+  [Table("Enou_FlowDynamicUser")]
+  public class FlowDynamicUser
+  { 
+    [Key]
+    public int flowDynamicUserId { get; set; }
+    public string guid { get; set; } = Guid.NewGuid().ToString();
+    [Required]
+    public string name { get; set; }
+    public string displayName { get; set; } //可用于前台友好显示
+    public string memo { get; set; } // 注释
+    public string script { get; set; }
+    public DateTime createTime { get; set; } = DateTime.Now;
+    public bool isVisible { get; set; } = true;
+    public bool isValidated { get; set; } = false;
+    public bool isPublished { get; set; } = false;
+    #region 自定义字段列表
+    public int? intField_1 { get; set; }
+    public int? intField_2 { get; set; }
+    public int? intField_3 { get; set; }
+    public int? intField_4 { get; set; }
+    public int? intField_5 { get; set; }
+    public int? intField_6 { get; set; }
+    public int? intField_7 { get; set; }
+    public int? intField_8 { get; set; }
+    public int? intField_9 { get; set; }
+    public int? intField_10 { get; set; }
+    public string stringField_1 { get; set; }
+    public string stringField_2 { get; set; }
+    public string stringField_3 { get; set; }
+    public string stringField_4 { get; set; }
+    public string stringField_5 { get; set; }
+    public string stringField_6 { get; set; }
+    public string stringField_7 { get; set; }
+    public string stringField_8 { get; set; }
+    public string stringField_9 { get; set; }
+    public string stringField_10 { get; set; }
+    public decimal? decimalField_1 { get; set; }
+    public decimal? decimalField_2 { get; set; }
+    public decimal? decimalField_3 { get; set; }
+    public decimal? decimalField_4 { get; set; }
+    public decimal? decimalField_5 { get; set; }
+    public decimal? decimalField_6 { get; set; }
+    public decimal? decimalField_7 { get; set; }
+    public decimal? decimalField_8 { get; set; }
+    public decimal? decimalField_9 { get; set; }
+    public decimal? decimalField_10 { get; set; }
+    public DateTime? dateTimeField_1 { get; set; }
+    public DateTime? dateTimeField_2 { get; set; }
+    public DateTime? dateTimeField_3 { get; set; }
+    public DateTime? dateTimeField_4 { get; set; }
+    public DateTime? dateTimeField_5 { get; set; }
+    #endregion
+  }
 }
