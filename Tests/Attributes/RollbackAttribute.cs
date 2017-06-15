cec49b49233e019ae609f182323881e3e02ece49
shibamo <qinchao@hotmail.com>
2017-06-15 16:07:31 -0700

    Setup environment for testing

---
+++ Tests/Attributes/RollbackAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace EnouFlowTemplateLib.Tests.Attributes
+{
+  /// <summary>
+  /// Rollback Attribute wraps test execution into a transaction and cancels the transaction once the test is finished.
+  /// You can use this attribute on single test methods or test classes/suites
+  /// </summary>
+  public class RollbackAttribute : Attribute, ITestAction
+  {
+    private TransactionScope transaction;
+
+    public void BeforeTest(ITest testDetails)
+    {
+      transaction = new TransactionScope();
+    }
+
+    public void AfterTest(ITest testDetails)
+    {
+      transaction.Dispose();
+    }
+
+    public ActionTargets Targets
+    {
+      get { return ActionTargets.Test; }
+    }
+  }
+}
---
+++ Tests/Integration/Template_TestsI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using EnouFlowTemplateLib.Tests.Attributes;
+using NSubstitute;
+
+namespace EnouFlowTemplateLib.Tests.Integration
+{
+  [TestFixture]
+  public class Template_TestsI
+  {
+
+  }
+}
